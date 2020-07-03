13945437b9ecac7008935b020d4563496c7c1c2d
victorguirra <victor.hugo.guirra@gmail.com>
2020-07-03 08:12:45 -0300

    Person App Screen

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'react-native';
+import App from './index';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function Main(){
+  return(
+    <>
+      <StatusBar backgroundColor="#03142B" barStyle="light-content"/>
+      <App />
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
