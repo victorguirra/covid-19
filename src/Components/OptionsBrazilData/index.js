9f41e80c75095d834cb32bb13a454c21ac044a47
victorguirra <victor.hugo.guirra@gmail.com>
2020-07-03 16:06:44 -0300

    Finished Add API

--- src/Components/OptionsBrazilData/index.js
+++ src/Components/OptionsBrazilData/index.js
@@ -1,16 +1,33 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-import {Container} from './styles';
+import {Container, Button, Title, Option} from './styles';
 
 export default function OptionsBrazilData(){
-     
+    const [apiData, setApiData] = useState([]);
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            const apiURL = 'https://covid19-brazil-api.now.sh/api/report/v1';
+            fetch(apiURL)
+            .then((response) => response.json())
+            .then((data) => setApiData(data));
+
+        }
+        fetchData();
+    },[])
+
     return(
 
         <Container>
 
-            
+            <Button onPress={() => console.log(apiData)}>
 
+                <Title>Show Data</Title>
+
+            </Button>
+           
         </Container>
         
     )
-}
+}   
--- src/Components/OptionsBrazilData/styles.js
+++ src/Components/OptionsBrazilData/styles.js
@@ -1,3 +1,14 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View``;
+
+export const Option = styled.View``;
+
+export const Button = styled.TouchableOpacity`
+    background-color:#ff1493;
+    padding:20px;
+`;
+
+export const Title = styled.Text`
+    color:#fff;
+`;
